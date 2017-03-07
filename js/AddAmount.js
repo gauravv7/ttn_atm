54925788fa0b4df172c4d6debc35a71b3045fc85
Ajay <ajaygoyal429@gmail.com>
2017-03-07 15:18:56 +0530

    Amount class modified

--- js/AddAmount.js
+++ js/AddAmount.js
@@ -5,14 +5,15 @@
 var amountAvailable;
 
 function  addAmount() {
-    var hundred = document.getElementById("hundred");
-    var five_hundred = document.getElementById("fiveHundred");
-    var two_thousand = document.getElementById("twoThousand");
-    amountAvailable=new Amount();
+    var hundred = document.getElementById("hundred").value;
+    var five_hundred = document.getElementById("fiveHundred").value;
+    var two_thousand = document.getElementById("twoThousand").value;
+    amountAvailable = new Amount();
 
     amountAvailable.hundred_rs_note=hundred;
     amountAvailable.fivehundred_rs_note=five_hundred;
     amountAvailable.twothousand_rs_note=two_thousand;
+    amountAvailable.amount=hundred*100 + five_hundred*500 + two_thousand*2000;
 
-
+    document.getElementById("statCurrentAmount").innerHTML="Current Amount: "  + amountAvailable.amount;
 }
--- js/enterAmount.js
+++ js/enterAmount.js
@@ -1,20 +1,18 @@
 
-//var r=require('./Singleton')
-
 function withdrawal() {
     var amount = document.getElementById("withdraw").value;
     var status=verify(amount);
-        alert (status);
+
 
 }
 
 function verify(amount) {
-    var x = new Amount();
+    var x = amountAvailable;
     var temp_transaction = new Amount();
     temp_transaction.amount = amount;
 
 
-    if (temp_transaction.amount > x.amount) {
+    if (temp_transaction.amount > amountAvailable.amount) {
         return "Insufficiant Balance";
     } else {
 
@@ -30,27 +28,29 @@ function verify(amount) {
         temp_transaction.hundred_rs_note = Math.trunc(temp_transaction.amount / 100);
         temp_transaction.amount -= temp_transaction.hundred_rs_note * 100;
 
-        if (x.twothousand_rs_note < temp_transaction.twothousand_rs_note) {
+        if (amountAvailable.twothousand_rs_note < temp_transaction.twothousand_rs_note) {
             temp_transaction.five_hundred += 4 * (temp_transaction.twothousand_rs_note - x.twothousand_rs_note);
-            if (!checkNotes(x, temp_transaction)) {
+            if (!checkNotes(temp_transaction)) {
 
                 return "Invalid Combination";
             }
         } else {
-            if (!checkNotes(x, temp_transaction)) {
+            if (!checkNotes(temp_transaction)) {
                 return "Invalid Combination";
             }
         }
     }
+
+
     return "verified";
 }
 
 
-function checkNotes(x, temp_transaction) {
-    if (x.fivehundred_rs_note < temp_transaction.fivehundred_rs_note) {
-        temp_transaction.hundred_rs_note += 5 * (temp_transaction.fivehundred_rs_note - x.fivehundred_rs_note);
+function checkNotes( temp_transaction) {
+    if (amountAvailable.fivehundred_rs_note < temp_transaction.fivehundred_rs_note) {
+        temp_transaction.hundred_rs_note += 5 * (temp_transaction.fivehundred_rs_note - amountAvailable.fivehundred_rs_note);
     }
 
-    if (x.hundred_rs_note < temp_transaction.hundred_rs_note) return false;
+    if (amountAvailable.hundred_rs_note < temp_transaction.hundred_rs_note) return false;
     return true;
 }
